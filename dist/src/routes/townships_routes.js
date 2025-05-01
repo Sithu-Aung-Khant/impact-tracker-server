2cbf4bb32cff0beac17ab456d86b31f5bd675a7f
Sithu <sithuaungkhant7825@gmail.com>
2025-05-01 22:39:46 +0630

    Remove deprecated seed, controller, route, and data files from the project, along with the direct URL from the Prisma schema, to streamline the codebase and eliminate unused components.

--- dist/src/routes/townships_routes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const township_controllers_1 = require("../controllers/township_controllers");
-const router = (0, express_1.Router)();
-router.get('/', township_controllers_1.getTownships);
-router.get('/support-stats', township_controllers_1.getTownshipSupportStats);
-exports.default = router;
